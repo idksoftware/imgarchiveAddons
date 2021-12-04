72e681fa3083c1687c83fc9120e5deeba9f66537
Iain Ferguson <i.ferguson@idk.co.uk>
2021-12-04 00:31:11 +0000

    updates

--- iashell/iachkout/CheckOutMultiForm.Designer.cs
+++ iashell/iachkout/CheckOutMultiForm.Designer.cs
@@ -40,9 +40,7 @@ namespace iachkout
             this.buttonImport = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonProperties = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBoxImport.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBoxImport
@@ -51,15 +49,10 @@ namespace iachkout
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBoxImport.Controls.Add(this.listViewImportFiles);
-            this.groupBoxImport.Controls.Add(this.buttonImport);
-            this.groupBoxImport.Controls.Add(this.buttonCancel);
-            this.groupBoxImport.Controls.Add(this.buttonProperties);
-            this.groupBoxImport.Location = new System.Drawing.Point(48, 93);
-            this.groupBoxImport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBoxImport.Location = new System.Drawing.Point(12, 12);
             this.groupBoxImport.Name = "groupBoxImport";
-            this.groupBoxImport.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBoxImport.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.groupBoxImport.Size = new System.Drawing.Size(910, 756);
+            this.groupBoxImport.Size = new System.Drawing.Size(651, 509);
             this.groupBoxImport.TabIndex = 9;
             this.groupBoxImport.TabStop = false;
             this.groupBoxImport.Text = "Selected items for Check-out";
@@ -76,10 +69,10 @@ namespace iachkout
             this.columnHeaderSize,
             this.columnHeaderFolder});
             this.listViewImportFiles.HideSelection = false;
-            this.listViewImportFiles.Location = new System.Drawing.Point(36, 63);
-            this.listViewImportFiles.Margin = new System.Windows.Forms.Padding(45, 31, 45, 46);
+            this.listViewImportFiles.Location = new System.Drawing.Point(15, 27);
+            this.listViewImportFiles.Margin = new System.Windows.Forms.Padding(30, 20, 30, 30);
             this.listViewImportFiles.Name = "listViewImportFiles";
-            this.listViewImportFiles.Size = new System.Drawing.Size(822, 424);
+            this.listViewImportFiles.Size = new System.Drawing.Size(622, 458);
             this.listViewImportFiles.TabIndex = 0;
             this.listViewImportFiles.UseCompatibleStateImageBehavior = false;
             this.listViewImportFiles.View = System.Windows.Forms.View.Details;
@@ -112,10 +105,9 @@ namespace iachkout
             // buttonImport
             // 
             this.buttonImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonImport.Location = new System.Drawing.Point(731, 681);
-            this.buttonImport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonImport.Location = new System.Drawing.Point(577, 539);
             this.buttonImport.Name = "buttonImport";
-            this.buttonImport.Size = new System.Drawing.Size(129, 45);
+            this.buttonImport.Size = new System.Drawing.Size(86, 29);
             this.buttonImport.TabIndex = 6;
             this.buttonImport.Text = "Check-out";
             this.buttonImport.UseVisualStyleBackColor = true;
@@ -124,10 +116,9 @@ namespace iachkout
             // buttonCancel
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonCancel.Location = new System.Drawing.Point(550, 681);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonCancel.Location = new System.Drawing.Point(477, 539);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(118, 45);
+            this.buttonCancel.Size = new System.Drawing.Size(79, 29);
             this.buttonCancel.TabIndex = 7;
             this.buttonCancel.Text = "Done";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -136,40 +127,27 @@ namespace iachkout
             // buttonProperties
             // 
             this.buttonProperties.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonProperties.Location = new System.Drawing.Point(36, 681);
-            this.buttonProperties.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonProperties.Location = new System.Drawing.Point(12, 539);
             this.buttonProperties.Name = "buttonProperties";
-            this.buttonProperties.Size = new System.Drawing.Size(142, 45);
+            this.buttonProperties.Size = new System.Drawing.Size(95, 29);
             this.buttonProperties.TabIndex = 8;
             this.buttonProperties.Text = "Properties";
             this.buttonProperties.UseVisualStyleBackColor = true;
             this.buttonProperties.Click += new System.EventHandler(this.buttonProperties_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(48, 37);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(48, 46);
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
-            // 
             // CheckOutMultiForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1013, 893);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(675, 580);
             this.Controls.Add(this.groupBoxImport);
+            this.Controls.Add(this.buttonProperties);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.buttonCancel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CheckOutMultiForm";
             this.Text = "Check-out - ImgArchive";
-            
             this.groupBoxImport.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -186,7 +164,6 @@ namespace iachkout
         private System.Windows.Forms.Button buttonProperties;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonImport;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
